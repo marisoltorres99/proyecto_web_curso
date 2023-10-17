751b5af2afc80893e9de943ac1c90ad61361cc1f
marisol <marisooltorrees@gmail.com>
2023-10-17 18:47:50 -0300

    mostrar servicios recuperandolos de la bd

--- tienda_web_curso/proyecto_web_app/views.py
+++ tienda_web_curso/proyecto_web_app/views.py
@@ -1,11 +1,14 @@
 from django.shortcuts import HttpResponse, render
+from servicios_app.models import Servicio
 
 
 def home(request):
     return render(request, "proyecto_web_app/home.html")
 
 def servicios(request):
-    return render(request, "proyecto_web_app/servicios.html")
+
+    servicios=Servicio.objects.all()
+    return render(request, "proyecto_web_app/servicios.html", {"servicios":servicios})
 
 def tienda(request):
     return render(request, "proyecto_web_app/tienda.html")
