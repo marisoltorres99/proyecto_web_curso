f66d5d6c1eff75849cf18174df0c0028425c9c8e
marisol <marisooltorrees@gmail.com>
2023-10-18 16:36:29 -0300

    muestro posts del blog

--- tienda_web_curso/blog_app/views.py
+++ tienda_web_curso/blog_app/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
 
+from blog_app.models import Post
+
 
 def blog(request):
-    return render(request, "blog_app/blog.html")
+    posts=Post.objects.all()
+    return render(request, "blog_app/blog.html", {"posts": posts})
