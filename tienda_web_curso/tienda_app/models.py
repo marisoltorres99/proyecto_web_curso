bc16bb95cb8acd2198c3dabc00bc5e641c2163fc
marisol <marisooltorrees@gmail.com>
2023-11-07 12:40:51 -0300

    creacion modelos categoria y producto

--- tienda_web_curso/tienda_app/models.py
+++ tienda_web_curso/tienda_app/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
-# Create your models here.
+
+class Categoria(models.Model):
+    nombre = models.CharField(max_length=50)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        verbose_name = "categoria"
+        verbose_name_plural = "categorias"
+
+    def __str__(self):
+        return self.nombre
+
+
+class Producto(models.Model):
+    nombre = models.CharField(max_length=50)
+    categorias = models.ForeignKey(Categoria, on_delete=models.CASCADE)
+    imagen = models.ImageField(upload_to="tienda_app", null=True, blank=True)
+    precio = models.FloatField()
+    disponibilidad = models.BooleanField(default=True)
+
+    class Meta:
+        verbose_name = "producto"
+        verbose_name_plural = "productos"
