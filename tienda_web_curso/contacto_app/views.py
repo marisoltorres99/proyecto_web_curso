65dd34bbe79a67ef310b53a4e7ae8de9cd54bee6
marisol <marisooltorrees@gmail.com>
2023-11-06 21:42:33 -0300

    envio de form

--- tienda_web_curso/contacto_app/views.py
+++ tienda_web_curso/contacto_app/views.py
@@ -1,8 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 
 from contacto_app.forms import FormularioContacto
 
 
 def contacto(request):
     formulario_contacto = FormularioContacto()
+    if request.method == "POST":
+        formulario_contacto = FormularioContacto(request.POST)
+        if formulario_contacto.is_valid():
+            nombre = request.POST.get("nombre")
+            email = request.POST.get("email")
+            contenido = request.POST.get("contenido")
+            return redirect("/contacto/?valido")
+
     return render(request, "contacto_app/contacto.html", {"form": formulario_contacto})
