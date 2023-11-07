1c4da98f4a7a997997eac328a448fb7733720d0b
marisol <marisooltorrees@gmail.com>
2023-11-07 18:24:19 -0300

    cambios modelo producto, registro en panel admin, cambio formato template

--- tienda_web_curso/tienda_app/admin.py
+++ tienda_web_curso/tienda_app/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Categoria, Producto
+
+
+class CategoriaAdmin(admin.ModelAdmin):
+    readonly_fields = ("created", "updated")
+
+
+class ProductoAdmin(admin.ModelAdmin):
+    readonly_fields = ("created", "updated")
+
+
+admin.site.register(Categoria, CategoriaAdmin)
+
+
+admin.site.register(Producto, ProductoAdmin)
--- tienda_web_curso/tienda_app/views.py
+++ tienda_web_curso/tienda_app/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
 
+from .models import Producto
+
 
 def tienda(request):
-    return render(request, "tienda_app/tienda.html")
+    productos = Producto.objects.all()
+    return render(request, "tienda_app/tienda.html", {"productos": productos})
