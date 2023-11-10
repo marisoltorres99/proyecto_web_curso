0c37946750f50144805a58f8dd8ac779b2ee3848
marisol <marisooltorrees@gmail.com>
2023-11-09 23:20:30 -0300

    creo vistas para llamar funciones de clase Carro y actualizo template tienda

--- tienda_web_curso/carro_app/views.py
+++ tienda_web_curso/carro_app/views.py
@@ -1,3 +1,42 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+from tienda_app.models import Producto
 
-# Create your views here.
+from .carro import Carro
+
+
+def agregar_producto(request, producto_id):
+    carro = Carro(request)
+
+    producto = Producto.objects.get(id=producto_id)
+
+    carro.agregar(producto=producto)
+
+    return redirect("tienda")
+
+
+def eliminar_producto(request, producto_id):
+    carro = Carro(request)
+
+    producto = Producto.objects.get(id=producto_id)
+
+    carro.eliminar(producto=producto)
+
+    return redirect("tienda")
+
+
+def restar_producto(request, producto_id):
+    carro = Carro(request)
+
+    producto = Producto.objects.get(id=producto_id)
+
+    carro.restar(producto=producto)
+
+    return redirect("tienda")
+
+
+def limpiar_carro(request, producto_id):
+    carro = Carro(request)
+
+    carro.limpiar()
+
+    return redirect("tienda")
