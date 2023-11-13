10cb9fe29018ced210768e348a570af4faa8185d
marisol <marisooltorrees@gmail.com>
2023-11-13 16:56:22 -0300

    creacion de context processor para guardar precio total del carrito

---
+++ tienda_web_curso/carro_app/context_processor.py
@@ -0,0 +1,7 @@
+def importe_total_carro(request):
+    total = 0
+    if request.user.is_authenticated:
+        carro = request.session.get("carro", {})
+        for key, value in carro.items():
+            total = total + (float(value["precio"]) * value["cantidad"])
+    return {"importe_total_carro": total}
