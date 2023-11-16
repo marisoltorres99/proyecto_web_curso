af7b6726f609ad13d0176fde8541ac7ecfb26dd6
marisol <marisooltorrees@gmail.com>
2023-11-16 13:52:50 -0300

    envio de form, registro de usuario en bd y en caso de error muestro mensaje

--- tienda_web_curso/autenticacion_app/views.py
+++ tienda_web_curso/autenticacion_app/views.py
@@ -1,5 +1,7 @@
+from django.contrib import messages
+from django.contrib.auth import login
 from django.contrib.auth.forms import UserCreationForm
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from django.views.generic import View
 
 
@@ -9,4 +11,13 @@ class VRegistro(View):
         return render(request, "registro/registro.html", {"form": form})
 
     def post(self, request):
-        pass
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            usuario = form.save()
+            login(request, usuario)
+            return redirect("Home")
+        else:
+            for msg in form.error_messages:
+                messages.error(request, form.error_messages[msg])
+
+            return render(request, "registro/registro.html", {"form": form})
