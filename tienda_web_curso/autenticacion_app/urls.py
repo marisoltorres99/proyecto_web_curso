35e920bd14cc6611465da586fb2afb144a720118
marisol <marisooltorrees@gmail.com>
2023-11-17 17:00:53 -0300

    creo form iniciar sesion y valido datos

--- tienda_web_curso/autenticacion_app/urls.py
+++ tienda_web_curso/autenticacion_app/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from .views import VRegistro, cerrar_sesion
+from .views import VRegistro, cerrar_sesion, iniciar_sesion
 
 urlpatterns = [
     path("", VRegistro.as_view(), name="autenticacion"),
+    path("iniciar_sesion", iniciar_sesion, name="iniciarsesion"),
     path("cerrar_sesion", cerrar_sesion, name="cerrar"),
 ]
--- tienda_web_curso/autenticacion_app/views.py
+++ tienda_web_curso/autenticacion_app/views.py
@@ -1,6 +1,6 @@
 from django.contrib import messages
-from django.contrib.auth import login, logout
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
 from django.shortcuts import redirect, render
 from django.views.generic import View
 
@@ -26,3 +26,22 @@ class VRegistro(View):
 def cerrar_sesion(request):
     logout(request)
     return redirect("Home")
+
+
+def iniciar_sesion(request):
+    if request.method == "POST":
+        form = AuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            nombre_usuario = form.cleaned_data.get("username")
+            contra = form.cleaned_data.get("password")
+            usuario = authenticate(username=nombre_usuario, password=contra)
+            if usuario is not None:
+                login(request, usuario)
+                return redirect("Home")
+            else:
+                messages.error(request, "Usuario No Válido")
+        else:
+            messages.error(request, "Informacion Incorrecta")
+
+    form = AuthenticationForm()
+    return render(request, "iniciar_sesion/iniciar_sesion.html", {"form": form})
