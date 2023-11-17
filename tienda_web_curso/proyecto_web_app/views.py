9af6cf2a91a42ad47cca7cbdcf1bb88492ef2cec
marisol <marisooltorrees@gmail.com>
2023-11-17 18:03:24 -0300

    monstrar mensaje en la tienda si no se inicio sesion

--- tienda_web_curso/proyecto_web_app/views.py
+++ tienda_web_curso/proyecto_web_app/views.py
@@ -1,5 +1,7 @@
+from carro_app.carro import Carro
 from django.shortcuts import HttpResponse, render
 
 
 def home(request):
+    carro = Carro(request)
     return render(request, "proyecto_web_app/home.html")
