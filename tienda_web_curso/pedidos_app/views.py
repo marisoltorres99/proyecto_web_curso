619e40b013eefc3a48ec3b6a0746ff8f2c5b1b17
marisol <marisooltorrees@gmail.com>
2023-11-20 18:32:27 -0300

    registro app pedido en admin, boton comprar, creacion de vista para procesar pedido

--- tienda_web_curso/pedidos_app/views.py
+++ tienda_web_curso/pedidos_app/views.py
@@ -1,3 +1,29 @@
-from django.shortcuts import render
+from carro_app.carro import Carro
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import redirect, render
 
-# Create your views here.
+from pedidos_app.models import LineaPedido, Pedido
+
+
+@login_required(login_url="/autenticacion_app/iniciar_sesion")
+def procesar_pedido(request):
+    pedido = Pedido.objects.create(user=request.user)
+    carro = Carro(request)
+    lineas_pedido = list()
+
+    for key, value in carro.carro.items():
+        lineas_pedido.append(
+            LineaPedido(
+                producto_id=key,
+                cantidad=value["cantidad"],
+                user=request.user,
+                pedido=pedido,
+            )
+        )
+
+    LineaPedido.objects.bulk_create(lineas_pedido)
+
+    messages.success(request, "El pedido se ha creaco correctamente")
+
+    return redirect("/tienda")
